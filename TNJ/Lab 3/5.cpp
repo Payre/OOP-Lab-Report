89869299659d2cee2fb8125502a91cdfd35d8c67
Payre <visionpeer@gmail.com>
2023-07-10 14:26:42 +0545

    lab report 5

--- TNJ/Lab 3/5.cpp
+++ TNJ/Lab 3/5.cpp
@@ -8,8 +8,43 @@ and
 had worked ... ... ... hours as overtime in the year ... ... ...*/
 #include <iostream>
 using namespace std;
+class employee
+{
+    int emp_id;
+    int year;
+    float bonus;
+    float overtime;
+    public:
+    void setpara(int emp_id, int year, float bonus, float overtime)
+    {
+        this->emp_id=emp_id;
+        this->year=year;
+        this->bonus=bonus;
+        this->overtime=overtime;
+    }
+
+    void displayreport()
+    {
+        cout<<"An employee with "<<emp_id<< "has received Rs "<<bonus<<" as a bonus and "<<"had worked "<<overtime<<" hours as overtime in the year "<< year<<endl;
+    }
+};
 
 int main()
 {
+    int n, emp_id, year;
+    float bonus, overtime;
+    cout<<"Enter the number of employee"<<endl;
+    cin>>n;
+    employee a[n];
+    for(int i=0;i<n;i++)
+    {
+        cout<<"Enter employee id, year, bonus and over time "<<i+1<<endl;
+        cin>>emp_id>>year>>bonus>>overtime;
+        a[i].setpara(emp_id, year, bonus, overtime);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        a[i].displayreport();
+    }
     return 0;
 }
