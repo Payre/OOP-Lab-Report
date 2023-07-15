63e03effe1cf179374f7cbc4b9edb3e55379a9ac
Payre <visionpeer@gmail.com>
2023-07-15 23:44:00 +0545

    lab 4 updated

---
+++ TNJ/lab_4/lab1.cpp
@@ -0,0 +1,80 @@
+/*Write a program that has a class to represent time.
+The class should have constructors to initialize data members hour, minute, and second to 0 and to initialize them to values passed as arguments.
+The class should have a member function to add time objects and return the result as a time object.
+There should be functions to display time in 12-hour and 24-hour format.*/
+#include <iostream>
+class Time
+{
+    int hour, minute, second;
+    public:
+    Time(int hour, int minute, int second); //prototyping, constructor that has arguments
+    Time()
+    {
+        hour = 0;
+        minute = 0;
+        second = 0;
+    }
+    Time(Time &b, Time&c)
+    {
+        std::cout<<"Enter the time: "<<std::endl;
+        std::cin>>b.hour>>b.minute>>b.second;
+        std::cout<<"Enter the time: "<<std::endl;
+        std::cin>>c.hour>>c.minute>>c.second;
+        
+    }
+    Time add(Time &d, Time &e)
+    {
+        Time time1;
+        time1.hour = d.hour + e.hour;
+        time1.minute = d.minute + e.minute;
+        time1.second = d.second + e.second;
+        std::cout<<time1.minute;
+        if (time1.second >= 60)
+        {
+            time1.second = time1.second%60;
+            time1.minute +=1;
+        }
+        if (time1.minute >= 60)
+        {
+            time1.minute = time1.minute%60;
+            time1.hour +=1;
+        }
+        return time1;
+    }
+    void Display12HourFormat()
+    {
+        if (hour>12)
+        {
+            std::cout << hour - 12<< " hour :" << minute << " minute :" << second << std::endl;
+        }
+    }
+    void Display24HourFormat()
+    {
+        std::cout << hour << "hour :" << minute << " minute :" << second << std::endl;
+    }
+
+};
+
+Time::Time(int hour, int minute, int second)
+{
+    this -> hour = hour;
+    this -> minute = minute;
+    this -> second = second;
+}
+
+int main()
+{
+
+    int hour, minute, second;
+    std::cout<<"Enter Hour, Minute, and Second: (for 1)"<<std::endl;
+    std::cin>>hour>>minute>>second;
+    Time t1(hour, minute, second);
+    std::cout<<"Enter Hour, Minute, and Second: (for 1)"<<std::endl;
+    std::cin>>hour>>minute>>second;
+    Time t2(hour, minute, second);
+    Time t3 = t3.add(t1, t2);
+
+    t1.Display12HourFormat();
+    t2.Display12HourFormat();
+    t3.Display24HourFormat();
+}
