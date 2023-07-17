a79d4db80c4bfc5ec10ec56d58559fc80bf6c57d
Payre <visionpeer@gmail.com>
2023-07-17 22:01:39 +0545

    lab 4 report 5 updated

--- TNJ/lab_4/5.cpp
+++ TNJ/lab_4/5.cpp
@@ -8,3 +8,43 @@ ii) const_object.const_mem_function
 iii) non_const_object.non_const_mem_function
 
 iv) non_const_object.const_mem_function*/
+
+#include <iostream>
+using namespace std;
+
+class A
+{
+    int x;
+public:
+    A()
+    {
+        x = 0;
+    }
+    void constdisplay() const
+    {
+        cout << "Constant member function" << endl;
+    }
+    void display()
+    {
+        cout << "Non-constant member function" << endl;
+    }
+};
+int main()
+{
+    const A a1; // Creating a const object of class A
+    A a2;       // Creating a non-const object of class A
+
+    // This line calls the non-const member function `display()` on the const object `a1`.
+    // But since a const object can only call const member functions, this line will give an error.
+    a1.display();
+
+    // This line calls the const member function `constdisplay()` on the const object `a1`.
+    a1.constdisplay();
+
+    // This line calls the const member function `constdisplay()` on the non-const object `a2`.
+    a2.display();
+
+    // This line calls the const member function `constdisplay()` on the non-const object `a2`.
+    a2.constdisplay();
+    return 0;
+}
