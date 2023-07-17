98a76f63da02b849bd1379cc1c73212e720372c1
Payre <visionpeer@gmail.com>
2023-07-17 21:47:31 +0545

    lab 4 report 5 added

---
+++ TNJ/lab_4/5.cpp
@@ -0,0 +1,10 @@
+/*
+    Write a program that illustrates the following relationship and comment on them. 
+
+i) const_object.non_const_mem_function
+
+ii) const_object.const_mem_function
+
+iii) non_const_object.non_const_mem_function
+
+iv) non_const_object.const_mem_function*/
