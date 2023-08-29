4c2e5e13aa812ab6e4e1dfeed07999f4fb2d2eb8
Payre <visionpeer@gmail.com>
2023-08-30 00:46:32 +0545

    lab repo 3 updated

--- TNJ/lab_5/3.cpp
+++ TNJ/lab_5/3.cpp
@@ -1,79 +1,73 @@
 /*Write a program to compare two objects of a class that contains an integer value as its data member.
 Make overloading functions to overload equality(==), less than(<), greater than(>), not equal (!=), greater than or equal to (>=),
-and less than or equal to(<=) operators using member operator functions.*/
+and less than or equal to(<=) operators using member operator functions.
+
+Reference: https://www.youtube.com/watch?v=smhS07tV5xg
+
+a => lhs, b=> rhs
+a.operator<(b)*/
 #include <iostream>
 using namespace std;
-class Complex
+class Square
 {
-    float real, imag;
+    double side;
+
     public:
-    Complex(): real(0), imag(0) {}
-    Complex(float real, float imag): real(real), imag(imag) {}
-    int operator==(Complex &a)
-    {
-        if (real = a.real)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    int operator<(Complex &a)
-    {
-        if(real<a.real)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    int operator>(Complex &a)
-    {
-        if(real>a.real)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    int operator>=(Complex &a)
-    {
-        if(real>=a.real)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
-    int operator<=(Complex &a)
-    {
-        if(real<=a.real)
-        {
-            return 1;
-        }
-        else
-        {
-            return 0;
-        }
-    }
+    Square(int x=0): side(x){}
+
+    //accessor
+    double getSide() const {return side;}
+    double getArea() const {return side * side;}
+    bool operator<(const Square& /*parameter, we are comparing not changing anything*/rhs) const;
+    bool operator==(const Square& rhs) const;
+    bool operator>(const Square& rhs) const;
+    bool operator!=(const Square& rhs) const;
+    bool operator>=(const Square& rhs) const;
+    bool operator<=(const Square& rhs) const;
 };
 
 int main()
 {
-    float x = 3, y =3, p=4, q=4;
-    Complex a(x, y), b(p,q);
+    Square a(20), b(20);
     if (a==b)
-    {
-        cout<<"They is equal";
-    }
-    else{cout<<"They are not equal";}
+        cout<<"a equal to b"<<endl;
+    else
+        cout<<"a is not equal to b"<<endl;
+    return 0;
 }
 
+//equal to (==)
+bool Square::operator==(const Square& rhs) const
+{
+    return getArea()==rhs.getArea();
+}
+
+//less than(<)
+bool Square::operator<(const Square& rhs) const
+{
+    return getArea()< rhs.getArea(); //this returns true or false
+}
+
+//greater than(>)
+bool Square::operator>(const Square& rhs) const
+{
+    return getArea()>rhs.getArea();
+}
+
+//not equal (!=)
+bool Square::operator!=(const Square& rhs) const
+{
+    return getArea()!=rhs.getArea();
+}
+
+//greater than or equal to (>=)
+bool Square::operator>=(const Square& rhs) const
+{
+    return getArea()>=rhs.getArea();
+}
+
+//less than or equal to(<=)
+bool Square::operator<=(const Square& rhs) const
+{
+    return getArea()<=rhs.getArea();
+}
